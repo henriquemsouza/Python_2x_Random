3a8b370a46eec31edb1249883e1ff57affec97b9
Henrique Martins <henrique.m_s@hotmail.com>
2017-08-10 10:36:14 -0300

    Create RegexwithPandasTime.py

---
+++ RegexwithPandasTime.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+time_sentences = ["Monday: The doctor's appointment is at 2:45pm.",
+                  "Tuesday: The dentist's appointment is at 11:30 am.",
+                  "Wednesday: At 7:00pm, there is a basketball game!",
+                  "Thursday: Be back home by 11:15 pm at the latest.",
+                  "Friday: Take the train at 08:10 am, arrive at 09:00am."]
+
+
+
+df=pd.DataFrame(time_sentences, columns=['text'])
+print(df['text'].str.extractall(r'((\d?\d):(\d\d) ?([ap]m))'))
